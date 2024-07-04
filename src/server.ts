5fceb2cbfa77e25a43e4e6c9a13f9c1c0113a9a6
More-Pe <peraltamorena.va@gmail.com>
2024-07-04 12:14:14 +0200

    feat: Updated token data

--- src/server.ts
+++ src/server.ts
@@ -29,13 +29,13 @@ app.post("/api/auth/login", login);
 // USERS CRUD
 app.get("/api/users", auth, getAllUsers); //A esto solo debe poder acceder el superadmin
 //TODO OBLIGATORIO ver perfil de usuario app.get("/api/users/profile",getUserProfile)
-app.put("/api/users/profile", updateUserById);
+app.put("/api/users/profile", auth, updateUserById);
 app.delete("/api/users/{id}", auth, deleteUserById); //A esto solo debe poder acceder el superadmin
 //TODO extra filtrar usuario por email (superadmin) app.get("/api/users?email=ejemplo@ejemplo.com", filterUserById)
 //TODO extra cambiar de roles (superadmin) app.put("/api/users/{id}/role", changeRoles)
 
 // SERVICES CRUD
 app.post("/api/services", auth, createService); //A esto solo debe poder acceder el superadmin
-app.get("/api/services", getAllServices);
+app.get("/api/services", auth, getAllServices);
 app.put("/api/services/{id}", auth, updateService); //A esto solo debe poder acceder el superadmin
 app.delete("/api/services/{id}", auth, deleteService); //A esto solo debe poder acceder el superadmin

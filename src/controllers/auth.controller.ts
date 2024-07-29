6f740f32bd27253d4244a6914f3409e250e6174f
More-Pe <peraltamorena.va@gmail.com>
2024-07-29 21:30:18 +0200

    feat: validate password format

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,6 +3,8 @@ import { Request, Response } from 'express';
 import { User } from '../database/models/User';
 import jwt from 'jsonwebtoken';
 
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 //CREATE
 export const register = async (req: Request, res: Response) => {
 	try {
@@ -18,7 +20,13 @@ export const register = async (req: Request, res: Response) => {
 			});
 		}
 
-		// TODO: Validate email format
+		// Validate email format
+		if (!emailRegex.test(email)) {
+			return res.status(400).json({
+				success: false,
+				message: 'Email format is not valid',
+			});
+		}
 
 		if (passwordHash.length < 8 || passwordHash.length > 12) {
 			return res.status(400).json({
@@ -51,38 +59,34 @@ export const register = async (req: Request, res: Response) => {
 	}
 };
 
-//LOG IN
-
+//LOGIN
 export const login = async (req: Request, res: Response) => {
 	try {
-		//1. Get information
+		// 1. Get information
 		const { email, password_hash } = req.body;
 
-		//2. Validate information
+		// 2. Validate information
 		if (!email || !password_hash) {
 			return res.status(400).json({
-				succes: false,
+				success: false,
 				message: 'Email and password are needed',
 			});
 		}
 
-		//3. Check if user exists
-
+		// 3. Check if user exists
 		const user = await User.findOne({
 			where: { email: email },
 		});
 
 		if (!user) {
 			return res.status(400).json({
-				succes: false,
+				success: false,
 				message: 'Email or password not valid',
 			});
 		}
-		//4. Chek password
-		const isPasswordValid = bcrypt.compareSync(
-			password_hash,
-			user.password_hash,
-		);
+
+		// 4. Check password
+		const isPasswordValid = bcrypt.compareSync(password_hash, user.password_hash);
 
 		if (!isPasswordValid) {
 			return res.status(400).json({
@@ -91,14 +95,14 @@ export const login = async (req: Request, res: Response) => {
 			});
 		}
 
-		//5. Create token
+		// 5. Create token
 		const token = jwt.sign(
 			{
 				id: user.id,
 				role_id: user.role_id,
 				email: user.email,
 			},
-			process.env.JWT_SECRET as string, //Important!
+			process.env.JWT_SECRET as string, // Ensure to replace with your JWT secret
 			{
 				expiresIn: '2h',
 			},
@@ -109,8 +113,6 @@ export const login = async (req: Request, res: Response) => {
 			message: 'User logged',
 			token: token,
 		});
-
-		console.log(user);
 	} catch (error) {
 		res.status(500).json({
 			success: false,
--- src/database/seeders/serviceSeeders.ts
+++ src/database/seeders/serviceSeeders.ts
@@ -1,48 +1,48 @@
 import { AppDataSource } from "../db";
 import { Service } from "../models/Service";
+
 export const serviceSeeder = async () => {
     try {
-        await AppDataSource.initialize()
+        await AppDataSource.initialize();
 
         const service1 = new Service();
         service1.id = 1;
-        service1.service_name = "Tatuajes personalizados";
-        service1.description = "Los clientes tendrán la libertad de seleccionar motivos y diseños únicos, personalizando completamente su experiencia de tatuaje de acuerdo a sus preferencias y gustos.";
-        service1.image_url="https://i.postimg.cc/sQ2JTGrW/personalizado.png"
+        service1.service_name = "Custom Tattoos";
+        service1.description = "Customers will have the freedom to select unique motifs and designs, completely customizing their tattoo experience according to their preferences and tastes.";
+        service1.image_url = "https://i.postimg.cc/sQ2JTGrW/personalizado.png";
         await service1.save();
-        
+
         const service2 = new Service();
         service2.id = 2;
-        service2.service_name = "Tatuajes del catálogo";
-        service2.description = "Ofrecemos la realización de tatuajes basados en diseños predefinidos en nuestro catálogo. Los clientes pueden elegir entre una variedad de opciones estilizadas y probadas.";
-        service2.image_url="https://i.postimg.cc/qtwLcX90/cata-logo.png"
+        service2.service_name = "Catalog Tattoos";
+        service2.description = "We offer the realization of tattoos based on predefined designs in our catalog. Customers can choose from a variety of stylized and tested options.";
+        service2.image_url = "https://i.postimg.cc/qtwLcX90/cata-logo.png";
         await service2.save();
-        
+
         const service3 = new Service();
         service3.id = 3;
-        service3.service_name = "Restauración y rejuvenecimiento de trabajos";
-        service3.description = "Nos especializamos en la restauración y rejuvenecimiento de tatuajes existentes. Nuestros expertos trabajan para mejorar y renovar tatuajes antiguos, devolviéndoles su vitalidad.";
-         service3.image_url="https://i.postimg.cc/bGpg2FV3/restauracio-n.png"
+        service3.service_name = "Tattoo Restoration and Rejuvenation";
+        service3.description = "We specialize in the restoration and rejuvenation of existing tattoos. Our experts work to enhance and renew old tattoos, restoring their vitality.";
+        service3.image_url = "https://i.postimg.cc/bGpg2FV3/restauracio-n.png";
         await service3.save();
-        
+
         const service4 = new Service();
         service4.id = 4;
-        service4.service_name = "Colocación de piercings y dilatadores";
-        service4.description = "Ofrecemos servicios profesionales para la colocación de piercings y dilatadores. Nuestro equipo garantiza procedimientos seguros y estilos variados para satisfacer las preferencias individuales de nuestros clientes.";
-         service4.image_url="https://i.postimg.cc/SnWVRpdn/piercings.png"
+        service4.service_name = "Piercing and Gauging Placement";
+        service4.description = "We offer professional services for the placement of piercings and gauges. Our team ensures safe procedures and various styles to meet the individual preferences of our clients.";
+        service4.image_url = "https://i.postimg.cc/SnWVRpdn/piercings.png";
         await service4.save();
-        
+
         const service5 = new Service();
         service5.id = 5;
-        service5.service_name = "Venta de piercings y otros artículos";
-        service5.description = "Además de nuestros servicios de aplicación, ofrecemos una selección de piercings y otros artículos relacionados con el arte.";
-        service5.image_url="https://i.postimg.cc/TLHJF2d4/joyeri-a.png"
+        service5.service_name = "Sale of Piercings and Other Items";
+        service5.description = "In addition to our application services, we offer a selection of piercings and other items related to the art.";
+        service5.image_url = "https://i.postimg.cc/TLHJF2d4/joyeri-a.png";
         await service5.save();
-        
+
         console.log("=======================================");
         console.log("Services seeder successfully");
         console.log("=======================================");
-
 
     } catch (error: any) {
         console.log("=======================================");
@@ -51,4 +51,4 @@ export const serviceSeeder = async () => {
     } finally {
         await AppDataSource.destroy();
     }
-}
+};
